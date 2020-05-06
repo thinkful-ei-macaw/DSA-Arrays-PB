c95d9625feb3a6e806428b5eadf490c9ffee4c16
Brannen Petit <bpetit940@gmail.com>
2020-05-06 11:45:09 -0500

    working on q 2

---
+++ array.js
@@ -0,0 +1,46 @@
+import memory from "./memory";
+
+class Array {
+  constructor() {
+    this.length = 0;
+    this._capacity = 0;
+    this.ptr = memory.allocate(this.length);
+  }
+
+  push(value) {
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * Array.SIZE_RATIO);
+    }
+
+    memory.set(this.ptr + this.length, value);
+    this.length++;
+  }
+
+  _resize(size) {
+    const oldPtr = this.ptr;
+    this.ptr = memory.allocate(size);
+    if (this.ptr === null) {
+      throw new Error("Out of memory");
+    }
+    memory.copy(this.ptr, oldPtr, this.length);
+    memory.free(oldPtr);
+    this._capacity = size;
+  }
+}
+Array.SIZE_RATIO = 3;
+
+function main() {
+  Array.SIZE_RATIO = 3;
+
+  // Create an instance of the Array class
+  let arr = new Array();
+
+  // Add an item to the array
+  arr.push(3);
+
+  console.log(arr);
+}
+
+main();
+
+module.exports = Array;
